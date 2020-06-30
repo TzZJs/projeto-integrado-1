0f189101a3fa536e7e843c3917178d02c5ecc04d
mbsd <mbsd@m0x.ru>
2020-06-29 21:06:20 -0300

    adicionado websocket

--- integrado.ino
+++ integrado.ino
@@ -3,10 +3,14 @@
 2. Ligar e desligar Leds
 3. Conectar Wifi
 4. Conectar no Servidor
+4.1 codigo hardware
+4.2 codigo servidor
 5. Enviaremos uma mensagem (vaga preenchida ou não)
 */
 
 #include <ESP8266WiFi.h>
+#include <Hash.h>
+#include <WebSocketsClient.h>
 
 /* wifi ssid */
 const char 	*wifi_ssid = "Murilo";
@@ -14,12 +18,21 @@ const char 	*wifi_ssid = "Murilo";
 const char 	*wifi_password = "32097070";
 
 /* GPIO LED Vermelho */
-int 		 LED_VERMELHO = 5;
-int		 LED_AMARELO = 4;
+const int	 LED_VERMELHO = 5;
+const int	 LED_AMARELO = 4;
 
 /* BaudRate 1152000 */
-int 		 BRD = 115200;
+const int 	 BRD = 115200;
 
+/* configurações para se conectar ao servidor websocket */
+const char	*ip_servidor = "192.168.15.38";
+const char	*rota = "/";
+const int	 porta = 1234;
+
+/* instância global websocket */
+WebSocketsClient websocket;
+
+static void fn_recebe_evento(WStype_t, uint8_t *, size_t);
 
 /*
  * Função Setup
@@ -51,6 +64,29 @@ setup(void)
 	/* retorna o ip da placa */
 	Serial.print("IP: ");
 	Serial.print(WiFi.localIP());
+
+	/* conectar ao servidor websocket */;
+	websocket.begin(ip_servidor, porta, rota);
+	/* qualquer evento */
+	websocket.onEvent(fn_recebe_evento);
+	/* intervalo de reconexão */
+	websocket.setReconnectInterval(5000);
+	/* heart beat */
+	websocket.enableHeartbeat(15000, 3000, 2);
+}
+
+static void
+fn_recebe_evento(WStype_t tipo, uint8_t *dado, size_t tamanho)
+{
+	switch(tipo) {
+		case WStype_DISCONNECTED:
+			Serial.println("Desconectado Websocket");
+			break;
+		case WStype_CONNECTED:
+			Serial.println("Conectado no Websocket");
+			websocket.sendTXT("TAMO CONECTADO!!!");
+			break;
+	}
 }
 
 /*
@@ -59,18 +95,20 @@ setup(void)
 void
 loop(void)
 {
+	websocket.loop();
 	/* Desligamos o led vermelho */
-	Serial.println("Desligando led vermelho");
+	/*Serial.println("Desligando led vermelho");
 	digitalWrite(LED_VERMELHO, LOW);
 	digitalWrite(LED_AMARELO, LOW);
 	delay(1000);
-
+*/
 	/* Liga o led vermelho */
+/*
 	Serial.println("Ligando led vermelho");
 	digitalWrite(LED_VERMELHO, HIGH);
 	digitalWrite(LED_AMARELO, HIGH);
 	delay(1000);
-
+*/
 	/* aguarde 2 segundos */
-	delay(2000);
+	//delay(2000);
 }
